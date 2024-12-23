b68ecf9fd803efabc27ed9ba46adeddcb1d65ef0
mahmoud sobhi <97081469+mahmoudSobhiAwad@users.noreply.github.com>
2024-12-23 16:06:39 +0200

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -3,15 +3,13 @@ import 'package:bank_account/check_account_id.dart';
 import 'package:bank_account/methods_to_do.dart';
 
 void main(List<String> arguments) {
-  print(" Welcom To Mahmoud's Bank !");
-  String name='omar';
-  print(name.contains('mr')?"found":'not');
-  // BankAccount? bankAccount = checkAccountId();
-  // if (bankAccount != null) {
-  //   differentOperations(bankAccount);
-  // } else {
-  //   print(
-  //       "Exit , you have exceed the max number of tries , try after 5 min ...");
-  // }
+ 
+   BankAccount? bankAccount = checkAccountId();
+   if (bankAccount != null) {
+     differentOperations(bankAccount);
+   } else {
+     print(
+         "Exit , you have exceed the max number of tries , try after 5 min ...");
+   }
 }
 
